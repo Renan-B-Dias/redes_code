b3ba0c3a652396d772abad860d67beadec441ebf
Renan Benatti Dias <renanbenattidias@gmail.com>
2017-10-23 18:33:57 -0300

    Final java

--- dns/java/Main.java
+++ dns/java/Main.java
@@ -1,8 +1,18 @@
-import java.net.UnknownHostException;
-import java.net.Inet6Address;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.InitialDirContext;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.Context;
 import java.net.InetAddress;
+import java.net.Inet6Address;
+import java.net.UnknownHostException;
+import java.util.Hashtable;
+
 
 public class Main {
+
+	private static final String MX = "MX";
+
 	public static void main(String[] args) {
 
 		if(args.length > 0) {
@@ -12,13 +22,50 @@ public class Main {
 				hostAddress = hostAddress.substring(0, hostAddress.length()-1);
 
 			try {
+
+				Hashtable<String, Object> enviroment = new Hashtable<>();
+	            enviroment.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.dns.DnsContextFactory");
+	            enviroment.put(Context.PROVIDER_URL, "dns:");
+
 				InetAddress[] addressReturn = Inet6Address.getAllByName(hostAddress);
-				for(InetAddress address: addressReturn) {
-					System.out.println(address);
+
+				if(addressReturn[0].getHostAddress() != null) {
+					String ipv4 = addressReturn[0].getHostAddress();
+					System.out.println("A: " + ipv4);
+				} else {
+					System.out.println("A: <none>");
 				}
 
-			} catch(UnknownHostException exception) {
-				System.out.println("UnknownHostException");
+				if(addressReturn[1].getHostAddress() != null) {
+					String ipv6 = addressReturn[1].getHostAddress();
+					System.out.println("AAAA: " + ipv6);
+				} else {
+					System.out.println("AAAA: <none>");
+				}
+
+				hostAddress = hostAddress.substring(4, hostAddress.length());
+
+				InetAddress inetAddress = addressReturn[0];
+				InitialDirContext initialDirContext = new InitialDirContext(enviroment);
+				Attributes attributes = initialDirContext.getAttributes(hostAddress, new String[] {MX});
+				
+				NamingEnumeration attributeEnumeration = attributes.getAll();
+
+				if(attributeEnumeration.hasMore())
+					while(attributeEnumeration.hasMore()) {
+						System.out.println("MX: " + attributeEnumeration.next());
+					}
+				else
+					System.out.println("MX: <none>");
+
+				attributeEnumeration.close();
+
+			}
+			catch(UnknownHostException exception) {
+				System.out.println(exception);
+			}
+
+			catch(NamingException exception) {
 				System.out.println(exception);
 			}
 
